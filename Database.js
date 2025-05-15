9ffa28e5d106e66d69eefd9513a7808ca733fae3
zaqen <zaqen1313@hotmail.com>
2025-05-15 13:19:56 +0200

    testing mysql på databasen

--- Database.js
+++ Database.js
@@ -2,15 +2,48 @@ const express = require("express")
 const fs = require('fs');
 const path = require('path');
 const dataPath = path.join(__dirname, 'table.json');
-
+const mysql = require('mysql2');
 
 const app = new express()
 const portNr = 8100
 app.use(express.json());
 
+// Anslutning till databasen
+const db = mysql.createConnection({
+  host: 'localhost',           // eller din VM IP om den körs i en container
+  user: 'ZaQen',
+  password: 'hejsan123',
+  database: 'hackers_db'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Kunde inte ansluta till databasen:', err);
+    process.exit(1);
+  }
+  console.log('Ansluten till MySQL');
+});
+
+// Funktion för att hämta all data som JSON
+function getHackers(callback) {
+  db.query('SELECT * FROM hackers', (err, results) => {
+    if (err) return callback(err, null);
+    callback(null, results); // results är redan ett JSON-kompatibelt objekt
+  });
+}
+
+module.exports = {
+  getHackers
+};
+
+
+
 app.get('/table', (req, res) => {
-  fs.readFile('./table.json', 'utf8', (err, data) => {
+  getHackers((err, data) => {
     if (err) {
+      console.error('Fel vid hämtning av data:', err);
+//  fs.readFile('./table.json', 'utf8', (err, data) => {
+//    if (err) {
       return res.status(500).send({ error: 'Kunde inte läsa användardata' });
     }
     res.setHeader('Content-Type', 'application/json');
@@ -47,3 +80,8 @@ app.post('/table', (req, res) => {
 app.listen(portNr, () => {
   console.log(`Databasserver kör på http://localhost:${portNr}`);
 });
+
+
+
+
+
